06909c368e34fb053bb51dde055ffb936334f570
Edsheebran <damien.armillon@student-cs.fr>
2020-05-09 10:48:46 +0200

    Display the grid

---
+++ src/components/Grid.vue
@@ -0,0 +1,52 @@
+<template>
+  <table>
+    <tr v-for="(row, rowIndex) in pattern" :key="`row-${rowIndex}`">
+      <td
+        v-for="(col, colIndex) in row"
+        :key="`cel-${rowIndex}-${colIndex}`"
+        :class="{alive:col}"
+      ></td>
+    </tr>
+  </table>
+</template>
+
+<style scoped>
+table,
+tr,
+td {
+  border: 0.1px solid black;
+  border-collapse: collapse;
+}
+
+table {
+  margin: auto;
+}
+
+td {
+  height: 0.5vmin;
+  width: 0.5vmin;
+}
+
+.alive {
+  background-color: black;
+}
+</style>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Grid extends Vue {
+  @Prop({ default: "HAHa" })
+  message!: string;
+
+  pattern: number[][] = [[]];
+
+  async created() {
+    const response = await fetch(
+      "https://thunder-dev.flashbrand.me/recruitment/life/gosperGliderGun"
+    );
+    this.pattern = (await response.json()).pattern;
+  }
+}
+</script>
